56c80bc074745568740c3b70e6437862bb32b3c2
davidingles <davidingleslopez@gmail.com>
2023-09-14 12:35:50 +0200

    puta madre biennnnn

---
+++ src/data/books.js
@@ -0,0 +1,38 @@
+export const books = [
+	{
+		id: 'B1Cerrada',
+		title: 'Bandeja Automontable: ',
+		img: '/catalogo-png/1L0024.png',
+		opinion: 'Bandeja Automontable: Diseño standard y basico de una bandeja automontable',
+		author: 'Auxiliar Manipuladora',
+		three: 'B1Cerrada'
+		// three: 'Model client:load'
+	},
+	{
+		id: 'BandejaAacotada',
+		title: 'Bandeja con Cotas',
+		img: '/catalogo-png/1P0221.png',
+		opinion: 'Bandeja automontable acotada.',
+		author: 'David Inglés',
+		three: 'BandejaAacotada'
+
+	},
+	{
+		id: 'EstucheConAsas',
+		title: 'Estuche con asas',
+		img: '/catalogo-png/Estuche tipo ANFORA.png',
+		opinion: '"Estuche practo para el transporte del usuario final',
+		author: 'Vicente Latorre',
+		three: 'EstucheConAsas'
+
+	},
+	{
+		id: 'ExpositorDM',
+		title: 'PLV y Expositores',
+		img: '/catalogo-png/3D EXPOSITOR DM.png',
+		opinion: '"Estuche practo para el transporte del usuario final',
+		author: 'Vicente Latorre',
+		three: 'ExpositorDM'
+
+	}
+]
